7f83abfcac9475cd88ebbf44fd3506480d9038be
leandroz <leandro.zubrezki@gmail.com>
2015-06-05 16:37:01 +1000

    Improve code for preventing nil exceptions, add showAddConnectionUI method.

--- src/ios/CordovaGigya.m
+++ src/ios/CordovaGigya.m
@@ -27,9 +27,17 @@
 // selectors
 - (void)notifiedOpenUrl:(NSNotification *)notification
 {
+
     NSLog(@"Url handled");
-    NSURL* url = (NSURL*)notification.object;
-    [Gigya handleOpenURL:url sourceApplication:nil annotation:nil];
+    NSLog(@"notification object: %@", notification.object);
+
+    //NSURL* url = (NSURL*)notification.object;
+    NSURL* url = [notification object];
+
+    if ([url isKindOfClass:[NSURL class]]) {
+        [Gigya handleOpenURL:url sourceApplication:nil annotation:nil];
+    }
+    
 }
 
 -(void)applicationDidBecomeActive:(UIApplication *)application
@@ -68,24 +76,37 @@
         completionHandler:^(GSUser *user, NSError *error) {
             CDVPluginResult* pluginResult = nil;
 
-            NSString* userString = [user JSONString];
-            NSData* userData = [userString dataUsingEncoding:NSUTF8StringEncoding];
-            NSDictionary* userDictionary = [NSJSONSerialization JSONObjectWithData:userData options:kNilOptions error:&error];
-
             if (!error) {
+                NSString* userString = [user JSONString];
+                NSData* userData = [userString dataUsingEncoding:NSUTF8StringEncoding];
+                NSDictionary* userDictionary = [NSJSONSerialization JSONObjectWithData:userData options:kNilOptions error:&error];
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userDictionary];
             }
             else {
                 // Handle error
                 NSLog(@"Login error: %@", error);
+                NSDictionary* userInfo = [error userInfo];
+                NSDictionary* data = nil;
 
-                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:userDictionary];
+                if(userInfo[@"state"] && userInfo[@"regToken"]) {
+                    data = @{
+                       @"state": [NSString stringWithString:userInfo[@"state"]],
+                       @"regToken": [NSString stringWithString:userInfo[@"regToken"]],
+                       @"errorCode": [NSNumber numberWithInteger:error.code],
+                       @"errorMessage": [NSString stringWithString:userInfo[@"NSLocalizedDescription"]]
+                    };
+                } else {
+                    data = @{
+                       @"errorCode": [NSNumber numberWithInteger:error.code],
+                       @"errorMessage": [NSString stringWithString:userInfo[@"NSLocalizedDescription"]]
+                    };
+                }
+
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:data];
             }
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 
         }];
-
-
 }
 
 - (void)login:(CDVInvokedUrlCommand*)command
@@ -117,13 +138,23 @@
                 NSLog(@"Login error: %@", error);
 
                 NSDictionary* userInfo = [error userInfo];
+                NSDictionary* data = nil;
 
-                NSDictionary* data = @{
-                   @"state": [NSString stringWithString:userInfo[@"state"]],
-                   @"regToken": [NSString stringWithString:userInfo[@"regToken"]],
-                   @"errorCode": [NSNumber numberWithInteger:error.code],
-                   @"errorMessage": [NSString stringWithString:userInfo[@"NSLocalizedDescription"]]
-                };
+                if(userInfo[@"state"] && userInfo[@"regToken"]) {
+                    data = @{
+                       @"state": [NSString stringWithString:userInfo[@"state"]],
+                       @"regToken": [NSString stringWithString:userInfo[@"regToken"]],
+                       @"errorCode": [NSNumber numberWithInteger:error.code],
+                       @"errorMessage": [NSString stringWithString:userInfo[@"NSLocalizedDescription"]]
+                    };
+                } else {
+                    data = @{
+                       @"errorCode": [NSNumber numberWithInteger:error.code],
+                       @"errorMessage": [NSString stringWithString:userInfo[@"NSLocalizedDescription"]]
+                    };
+                }
+
+                
 
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:data];
             }
@@ -273,6 +304,44 @@
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Already logged in. Logout first."];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }
+}
+
+- (void)showAddConnectionUI:(CDVInvokedUrlCommand*)command
+{
+    NSArray* providers;
+
+    if([command.arguments objectAtIndex:0] != [NSNull null]){
+        providers = [command.arguments objectAtIndex:0];
+    }
+    else {
+        providers = @[];
+    }
+
+    NSDictionary* params = nil;
+
+    if([command.arguments objectAtIndex:1] != [NSNull null]){
+        params = [command.arguments objectAtIndex:1];
+    }
+
+    [Gigya showAddConnectionProvidersDialogOver:super.viewController
+        providers:providers
+        parameters:params
+        completionHandler:^(GSUser *user, NSError *error) {
+            CDVPluginResult* pluginResult = nil;
+
+            NSString* userString = [user JSONString];
+            NSData* userData = [userString dataUsingEncoding:NSUTF8StringEncoding];
+            NSDictionary* userDictionary = [NSJSONSerialization JSONObjectWithData:userData options:kNilOptions error:&error];
+
+            if (!error) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userDictionary];
+            }
+            else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:userDictionary];
+            }
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+        }];
 }
 
 - (void)logout:(CDVInvokedUrlCommand*)command
